9de594a41da3e5647cdb556e8ecb3a211620ee97
Oleksandr Kushnir <xelambert@gmail.com>
2020-05-13 15:36:12 +0300

    Adding "Kushnir Aleksandr"

--- git_global/Program.cs
+++ git_global/Program.cs
@@ -20,7 +20,7 @@ namespace git_global
             Console.WriteLine("Yaroslav Pokutnii");
             Console.WriteLine("Alina_Poliukhovych 2");
             Console.WriteLine("Ivanov Aleksandr 2");
-
+            Console.WriteLine("Kushnir Aleksandr");
 
         }
     }
