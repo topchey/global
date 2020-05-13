7f980410a1a9f710e0a6927c36188898437cd53c
Alex Ivanov <alexandr83@gmail.com>
2020-05-13 12:01:11 +0300

    commit Ivanov

--- git_global/Program.cs
+++ git_global/Program.cs
@@ -13,6 +13,7 @@ namespace git_global
             Console.WriteLine("Pedagog");
             Console.WriteLine("Pedagog");
             Console.WriteLine("Alina");
+            Console.WriteLine("Ivanov");
 
         }
     }
